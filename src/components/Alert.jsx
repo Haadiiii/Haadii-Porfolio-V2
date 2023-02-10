d556dcb2a785e6f9541e0fd7be326f91ef8a70e4
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-02-11 02:25:29 +0500

    implement send eamil functionality and form validations.

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const Alert = ({ msg }) => {
-  return (
-    <div>
-      <div
-        class={`bg-blue-100 border-t border-b border-${color}-500 text-${color}-700 px-4 py-3`}
-        role="alert"
-      >
-        <p class="text-sm">{msg}</p>
-      </div>
+const Alert = ({ msg, setOpen }) => {
+  setTimeout(() => {
+    setOpen(false);
+  }, 2500);
+
+  return msg === "Your message has been sent" ? (
+    <div
+      className={`bg-green-300 border-t border-b border-green-500 text-green-700 px-4 py-3 mb-5`}
+      role="alert"
+    >
+      <p className="text-sm">{msg}</p>
+    </div>
+  ) : (
+    <div
+      className={`bg-orange-700 border-t border-b border-orange-500 text-orange-300 px-4 py-3 mb-5`}
+      role="alert"
+    >
+      <p className="text-sm">{msg}</p>
     </div>
   );
 };
--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -1,42 +1,65 @@
 import LineGradient from "../components/LineGradient";
-import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 import contact from "../assets/contact-image.jpeg";
 import { useState } from "react";
+import Alert from "../components/Alert";
 const Contact = () => {
-  const[values, setValues] = useState({
+  const [values, setValues] = useState({
     name: "",
     email: "",
     message: "",
-  })
+  });
+
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault();
-    Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "hammali99@gmail.com",
-      Password : "580996889DE4204A9578F0EB26EA982FD74A",
-      To : 'hammali99@gmail.com',
-      From : "hammali99@gmail.com",
-      Subject : "contact information",
-      Body : `Name: ${values.name} <br/> Email: ${values.email} <br/> Message: ${values.message}`
-  }).then(message => {
-    if(message === 'OK'){
-      alert('Your message has been sent, We will contact you shortly')
-    }else{
-      alert('There is error at sending message. ')
+    const regex = new RegExp('/^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/');
+
+    if (!values.name || !values.email || !values.message) {
+      setMsg("Please enter all required fields");
+      setOpen(true);
+      return;
     }
-  }
-  );
+
+    if (values.email !== values.email.toLowerCase()) {
+      setMsg("Please enter lowercase letters.");
+      setOpen(true);
+      return;
+    }
+
+    if (!regex.test(values.email)) {
+      setMsg("Invalid email address!");
+      setOpen(true);
+      return;
+    }
+
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "hammali99@gmail.com",
+      Password: "580996889DE4204A9578F0EB26EA982FD74A",
+      To: "hammali99@gmail.com",
+      From: "hammali99@gmail.com",
+      Subject: "contact information",
+      Body: `Name: ${values.name} <br/> Email: ${values.email} <br/> Message: ${values.message}`,
+    }).then((message) => {
+      if (message === "OK") {
+        setMsg("Your message has been sent");
+        setOpen(true);
+      } else {
+        setMsg("There is an error at sending message.");
+        setOpen(false);
+      }
+    });
   };
 
   const handleChange = (e) => {
     setValues({
       ...values,
-      [e.target.name]: e.target.value
-    })
-  }
-
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <section id="contact" className="contact py-40">
@@ -61,7 +84,6 @@ const Contact = () => {
           </div>
         </div>
       </motion.div>
-
       {/* FORM & IMAGE */}
       <div className="md:flex md:justify-between gap-16 mt-4">
         <motion.div
@@ -89,9 +111,8 @@ const Contact = () => {
           }}
           className="basis-1/2 mt-10 md:mt-0"
         >
-          <form
-            onSubmit={onSubmit}
-          >
+          {open && <Alert msg={msg} setOpen={setOpen} />}
+          <form onSubmit={onSubmit}>
             <input
               className="w-full bg-blue font-semibold placeholder-opaque-black p-3"
               type="text"
@@ -108,7 +129,6 @@ const Contact = () => {
               name="email"
               onChange={handleChange}
               value={values.email}
-              
             />
 
             <textarea
@@ -119,7 +139,6 @@ const Contact = () => {
               cols="50"
               onChange={handleChange}
               value={values.message}
-             
             />
 
             <button
